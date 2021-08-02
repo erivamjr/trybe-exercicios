17c0d1522ac6581864f335ca1ee392238d3406ce
José Erivam <erivamdev@gmail.com>
2021-08-02 18:24:22 -0300

    Adicionando aula course 4.4

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-slogica-de-programacao/aula-course-4.4.js
@@ -0,0 +1,20 @@
+//1 - Crie um objeto player contendo as variáveis listadas abaixo.
+/*let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };*/
+
+
+let player = {
+    name = 'Marta',
+    lastName = 'Silva',
+    age = 34,
+    medals = { golden: 2, silver: 3 }
+
+};
+
+/*2 - Acesse as chaves name , lastName e age e concatene as suas 
+informações para imprimir no console uma mensagem no seguinte 
+formato: "A jogadora Marta Silva tem 34 anos de idade".*/
+
+console.log('O meu nome é ' + player.name + ' e sobrenome é ' + player.lastName + ' com idade de ' + player.age);
