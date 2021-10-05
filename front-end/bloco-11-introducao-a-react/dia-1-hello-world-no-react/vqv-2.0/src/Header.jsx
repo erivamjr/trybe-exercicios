bd2c47ef5b82c232eb4c0e7154806cc8a84d3632
José Erivam <erivamdev@gmail.com>
2021-10-05 20:51:09 -0300

    adding exercise three completed

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/vqv-2.0/src/Header.jsx
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/vqv-2.0/src/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 class Header extends React.Component {
     render(){
         return (
-            <div>
+            <header className = 'header'>
                 <h1>Conteúdos de Front-End</h1>
-            </div>
+            </header>
         )
     }
 }
