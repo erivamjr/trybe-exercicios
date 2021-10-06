cf4e77dc108e9a2b1d8a2e29c57b56613f20f1aa
José Erivam <erivamdev@gmail.com>
2021-10-06 07:45:04 -0300

    adding folder and file of Components

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
+import PokemonShow from './Components/PokemonShow';
+
 import './App.css';
 
 function App() {
+  const pokemons = 
+    {
+        id: 25,
+        name: "Pikachu",
+        type: 'Electric',
+        averageWeight: {
+            value: 6.0,
+            measurementUnit: "kg"
+        },
+        image: "https:\/\/cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png",
+        moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)"
+    };
+
   return (
     <div className="App">
       <h1>My Pokedex</h1>
       <section>
         <article>
-        <h1>nome do pokemon;</h1>
-        <p>tipo do pokemon;</p>
-        <p>peso médio do pokemon, acompanhado da unidade de medida usada;</p>
-        <img src = '' alt='' />
+        <h1>{pokemons.name}</h1>
+        <p>{pokemons.type}</p>
+        <p>{`${pokemons.averageWeight.value} ${pokemons.averageWeight.measurementUnit}`}</p>
+        <img src ={pokemons.image} alt={pokemons.name} />
         </article>
       </section>
     </div>
