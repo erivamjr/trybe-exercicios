bd3d91214afb4e789c09ede0a9bfce3313ac9ebb
José Erivam <erivamdev@gmail.com>
2021-12-21 19:49:41 -0300

    adding exercese one

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -1,32 +1,15 @@
 import PokemonShow from './Components/PokemonShow';
+import pokemons from './data';
 
 import './App.css';
 
 function App() {
-  const pokemons = 
-    {
-        id: 25,
-        name: "Pikachu",
-        type: 'Electric',
-        averageWeight: {
-            value: 6.0,
-            measurementUnit: "kg"
-        },
-        image: "https:\/\/cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png",
-        moreInfo: "https:\/\/bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)"
-    };
+  
 
   return (
     <div className="App">
       <h1>My Pokedex</h1>
-      <section>
-        <article>
-        <h1>{pokemons.name}</h1>
-        <p>{pokemons.type}</p>
-        <p>{`${pokemons.averageWeight.value} ${pokemons.averageWeight.measurementUnit}`}</p>
-        <img src ={pokemons.image} alt={pokemons.name} />
-        </article>
-      </section>
+      <PokemonShow pokemons = {pokemons}/>
     </div>
   );
 }
