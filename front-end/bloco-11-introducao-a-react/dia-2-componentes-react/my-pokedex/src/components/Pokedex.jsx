d9f4f20adc63c0c99e7bacad9806e276c2a4c523
José Erivam <erivamdev@gmail.com>
2021-12-22 21:04:44 -0300

    adding exercise one full

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.jsx
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokedex.jsx
@@ -5,12 +5,12 @@ export class Pokedex extends React.Component {
     render() {
         const { pokemons } = this.props;
         return (
-            <div className="App">
-                <h1>My Pokedex</h1>
-                <section className="Pokedex">
-                    {pokemons.map((pokemon) => <Pokemon key={pokemon.id}pokemon={pokemon} />)}
-                </section>
-            </div>
+          <div className="App">
+            <h1>My Pokedex</h1>
+            <section className="Pokedex">
+              {pokemons.map((pokemon) => <Pokemon key={pokemon.id}pokemon={pokemon} />)}
+            </section>
+          </div>
         )
     }
 }
--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokemon.jsx
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokemon.jsx
@@ -4,16 +4,16 @@ class Pokemon extends React.Component {
   render() {
     const { pokemon } = this.props;
     return (
-          <article className="Pokemon">
-            <div>
-            <h1>{pokemon.name}</h1>
-            <p>{pokemon.type}</p>
-            <p>{`${pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`}</p>
-            </div>
-            <div>
-            <img src={pokemon.image} alt={pokemon.name} />
-            </div>
-          </article>
+      <article className="Pokemon">
+        <div>
+          <h1>{pokemon.name}</h1>
+          <p>{pokemon.type}</p>
+          <p>{`${pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`}</p>
+        </div>
+        <div>
+          <img src={pokemon.image} alt={pokemon.name} />
+        </div>
+      </article>
     );
   }
 }
