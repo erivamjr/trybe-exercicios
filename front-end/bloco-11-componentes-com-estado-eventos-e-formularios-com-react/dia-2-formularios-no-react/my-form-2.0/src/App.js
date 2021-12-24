a477e02b758f902042e88c07d65795a7e210e04e
José Erivam <erivamdev@gmail.com>
2021-12-24 19:49:59 -0300

    starting exercise one day two

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonalData from './components/PersonalData';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h2>Forms</h2>
+      </header>
+      <section>
+        <PersonalData/>
+      </section>
+      
+    </div>
+  );
+}
+
+export default App;
