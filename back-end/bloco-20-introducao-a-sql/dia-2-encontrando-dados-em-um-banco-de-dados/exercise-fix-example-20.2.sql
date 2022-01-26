f066984b2c7c2501e473ef43239daf724d181972
José Erivam <erivamdev@gmail.com>
2022-01-26 08:07:48 -0300

    adding exercise of the fixation example :boom: :boom: :boom:

--- back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercise-fix-example-20.2.sql
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercise-fix-example-20.2.sql
@@ -4,3 +4,5 @@ SELECT * FROM sakila.rental;
 SELECT * FROM sakila.rental LIMIT 10;
 # Query + LIMIT quantidade_de_linhas OFFSET quantidade_de_linhas
 SELECT * FROM sakila.rental LIMIT 10 OFFSET 3;
+SELECT * FROM sakila.address
+ORDER BY district ASC, address DESC;
