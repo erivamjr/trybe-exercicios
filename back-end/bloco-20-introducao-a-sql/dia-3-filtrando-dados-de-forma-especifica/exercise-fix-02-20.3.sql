84e2403024b02467411b20d9b51b7bb8be1ebef4
José Erivam <erivamdev@gmail.com>
2022-01-26 20:50:42 -0300

    adding exercise of the fixation two 2/7 :green_heart:

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-02-20.3.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-02-20.3.sql
@@ -1,3 +1,7 @@
 /*1-Mostre todos os detalhes dos filmes que contêm a palavra ace no nome.*/
 SELECT * FROM sakila.film
 WHERE title LIKE '%ace%';
+/*2-Mostre todos os detalhes dos filmes cujas descrições finalizam com china .*/
+SELECT * FROM sakila.film
+WHERE description LIKE '%china';
+/*3-*/
