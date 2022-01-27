3e063fe70a978e330cbb5c1513bf923881012002
José Erivam <erivamdev@gmail.com>
2022-01-27 15:07:44 -0300

    adding exercise of the fixation one 2/4 👫👬👭💪

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise-fix-01-20.4.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise-fix-01-20.4.sql
@@ -1,3 +1,4 @@
+/*1-Insira um novo funcionário na tabela sakila.staff .*/
 USE sakila;
 INSERT INTO staff (
 first_name, 
@@ -10,4 +11,20 @@ username,
 password
 ) VALUES ('José','Erivam',2,'erivam@hotmail.com',1,0,'junior', '123456');
 
+/*2-Feito o exercício anterior, vamos agora para o nível 2. Insira dois 
+funcionários novos em apenas uma query .*/
+INSERT INTO staff (
+first_name, 
+last_name, 
+address_id,
+email, 
+store_id, 
+active, 
+username,
+password
+) VALUES ('Geiza','Maia',2,'geiza@hotmail.com',1,0,'geiza', '123456'),
+('Davi','Ferreira',2,'davi@gmail.com',1,0,'mileras','123456');
+
+/*3-Selecione os cinco primeiros nomes e sobrenomes da tabela 
+sakila.customer e cadastre essas pessoas como atores na tabela sakila.actor .*/
 SELECT * FROM staff;
