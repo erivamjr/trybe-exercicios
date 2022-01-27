5f259f8366f57c0234219eb23003bbeefb6c9a8d
José Erivam <erivamdev@gmail.com>
2022-01-27 05:53:13 -0300

    adding exercise of the fixation three 2/5 :hocho: :skull:

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-03-20.3.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-03-20.3.sql
@@ -2,9 +2,12 @@
 crawford, henry, boyd, mason, morales e kennedy , ordenados por nome em ordem alfabética.*/
 SELECT first_name, last_name, email FROM sakila.customer
 WHERE last_name IN ('hicks', 
-'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy');
+'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
+ORDER BY first_name;
 /*2-Mostre o e-mail dos clientes com os address_id 172, 173, 174, 175 e 176 , ordenados 
 em ordem alfabética.*/
+SELECT * FROM sakila.customer
+WHERE address_id IN (172,173,174,175,176);
 /*3-Descubra quantos pagamentos foram feitos entre 01/05/2005 e 01/08/2005 . Lembre-se de 
 que, no banco de dados, as datas estão armazenadas no formato ano/mês/dia , diferente do 
 formato brasileiro, que é dia/mês/ano .*/
