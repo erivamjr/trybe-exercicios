deb60348072453e66b3a8ee0fc419ddd99a72258
José Erivam <erivamdev@gmail.com>
2022-01-27 05:44:35 -0300

    adding exercise of the fixation three 1/5 :whale:

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-03-20.3.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-03-20.3.sql
@@ -1,5 +1,8 @@
 /*1-Mostre o nome, sobrenome e e-mail dos clientes com os seguintes sobrenomes: hicks, 
 crawford, henry, boyd, mason, morales e kennedy , ordenados por nome em ordem alfabética.*/
+SELECT first_name, last_name, email FROM sakila.customer
+WHERE last_name IN ('hicks', 
+'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy');
 /*2-Mostre o e-mail dos clientes com os address_id 172, 173, 174, 175 e 176 , ordenados 
 em ordem alfabética.*/
 /*3-Descubra quantos pagamentos foram feitos entre 01/05/2005 e 01/08/2005 . Lembre-se de 
