70a6e17965b144aa997efa5e290110c05b66bf04
José Erivam <erivamdev@gmail.com>
2022-01-27 07:50:29 -0300

    adding exercises 2/7 🏃💨😋🍣😖

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-01-20.3.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-01-20.3.sql
@@ -1,6 +1,10 @@
 /*1-Escreva uma query para exibir todas as peças que começam com GR .*/
+SELECT * FROM PecasFornecedores.Pecas
+WHERE name LIKE 'GR%';
 /*2-Escreva uma query para exibir todos os fornecimentos que contenham 
 a peça com code 2 . Organize o resultado por ordem alfabética de fornecedor.*/
+SELECT * FROM PecasFornecedores.Fornecimentos
+WHERE peca LIKE 2 ORDER BY Fornecedor;
 /*3-Escreva uma query para exibir as peças, preço e fornecedor de todos os 
 fornecimentos em que o código do fornecedor tenha a letra N .*/
 /*4-Escreva uma query para exibir todas as informações dos fornecedores que 
