e9226753944da7218e4c5a4d4199ddce7d70d4da
José Erivam <erivamdev@gmail.com>
2022-01-27 07:19:33 -0300

    adding exercise of the fixation 3/4 :green_heart:

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-04-20.3.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-04-20.3.sql
@@ -7,5 +7,14 @@ SELECT COUNT(*) AS Resultado_total FROM sakila.payment
 WHERE payment_date BETWEEN '2005-07-01' AND '2005-08-22';
 /*3-Usando a tabela rental , extraia data, ano, mês, dia, hora, minuto e segundo dos 
 registros com rental_id = 10330. Utilize a coluna rental_date para extrair as informações.*/
+SELECT DATE(rental_date) AS Data,
+YEAR(rental_date) AS Ano,
+MONTH(rental_date) AS Mes,
+DAY(rental_date) AS Dia,
+HOUR(rental_date) AS Hora,
+MINUTE(rental_date) AS Minuto,
+SECOND(rental_date) AS Segundo
+ FROM sakila.rental
+WHERE rental_id LIKE 10330;
 /*4-Monte uma query que retorne todos os dados do pagamento feito no dia 28/07/2005 a 
 partir das 22 horas .*/
