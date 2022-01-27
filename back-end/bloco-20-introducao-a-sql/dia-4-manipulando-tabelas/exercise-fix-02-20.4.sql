4d95f3216d14ce96f733f8f299c1194e209710f2
José Erivam <erivamdev@gmail.com>
2022-01-27 20:52:25 -0300

    adding exercise of the fixation two 2/4 👫👬👭💪

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise-fix-02-20.4.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise-fix-02-20.4.sql
@@ -11,5 +11,13 @@ SELECT * FROM sakila.actor
 WHERE first_name LIKE 'JULIES';
 
 /*2-Foi exigido que a categoria "Sci-Fi" seja alterada para "Science Fiction".*/
+SELECT * FROM sakila.category;
+UPDATE sakila.category
+SET name = 'Science Fiction'
+WHERE name = 'Sci-Fi';
+SET SQL_SAFE_UPDATES = 0;
+
+SELECT * FROM sakila.category
+WHERE name = 'Science Fiction';
 /*3-Atualize o valor do aluguel para $25 de todos os filmes com duração maior que 100 minutos e que possuem a classificações "G" , "PG" ou "PG-13" e um custo de substituição maior que $20.*/
 /*4-Foi determinado pelo setor financeiro que haverá um reajuste em todos os preços dos filmes, com base em sua duração. Para todos os filmes com duração entre 0 e 100, o valor do aluguel passará a ser $10,00, e o aluguel dos filmes com duração acima de 100 passará a ser de $20,00.*/
