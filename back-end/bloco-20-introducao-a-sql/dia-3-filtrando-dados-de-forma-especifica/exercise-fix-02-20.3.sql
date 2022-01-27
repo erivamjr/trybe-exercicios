ec0a78d2554c288b4bf2cc72ba2b96e213fef9c8
José Erivam <erivamdev@gmail.com>
2022-01-26 21:15:39 -0300

    adding exercise of the fixation two 6/7 :green_heart:

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-02-20.3.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-fix-02-20.3.sql
@@ -18,4 +18,5 @@ palavra gon e a descrição contém a palavra Documentary .*/
 SELECT * FROM sakila.film
 WHERE title LIKE '%____gon%' AND description LIKE '%Documentary%';
 /*6-Mostre os dois filmes cujos títulos ou finalizam com academy ou iniciam com mosquito .*/
-/*7-Mostre os seis filmes que contêm as palavras monkey e sumo em suas descrições.*/
+SELECT * FROM sakila.film
+WHERE title LIKE '%academy' OR title LIKE 'mosquito%';
