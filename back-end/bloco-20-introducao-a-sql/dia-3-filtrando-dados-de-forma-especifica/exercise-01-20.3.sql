ab2c63b2367c70269e4633477fb1f1a109d14dbf
José Erivam <erivamdev@gmail.com>
2022-01-27 08:05:31 -0300

    adding exercises 5/7 ❌🐕🏹🐱

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-01-20.3.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise-01-20.3.sql
@@ -15,7 +15,8 @@ SELECT * FROM PecasFornecedores.Fornecedores
 WHERE name LIKE '%LTDA%' ORDER BY name DESC;
 /*5-Escreva uma query para exibir o número de empresas (fornecedores) que 
 contém a letra F no código.*/
-
+SELECT COUNT(*) FROM PecasFornecedores.Fornecedores
+WHERE name LIKE '%F%';
 /*6-Escreva uma query para exibir os fornecimentos onde as peças custam mais 
 de R$15,00 e menos de $40,00 . Ordene os resultados por ordem crescente.*/
 /*7-Escreva uma query para exibir o número de vendas feitas entre o dia 
