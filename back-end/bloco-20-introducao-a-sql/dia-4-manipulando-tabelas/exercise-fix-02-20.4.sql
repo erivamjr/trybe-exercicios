9b8dafd1bc182713ff8697d448d173a84157ecb2
José Erivam <erivamdev@gmail.com>
2022-01-27 21:24:44 -0300

    adding exercise of the fixation two 4/4 😱🔪

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise-fix-02-20.4.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise-fix-02-20.4.sql
@@ -30,3 +30,9 @@ SET SQL_SAFE_UPDATES = 0;
 SELECT * FROM sakila.film
 WHERE replacement_cost = 25 AND length > 100 AND rating <> 'NC-17';
 /*4-Foi determinado pelo setor financeiro que haverá um reajuste em todos os preços dos filmes, com base em sua duração. Para todos os filmes com duração entre 0 e 100, o valor do aluguel passará a ser $10,00, e o aluguel dos filmes com duração acima de 100 passará a ser de $20,00.*/
+SELECT * FROM sakila.film;
+UPDATE sakila.film
+SET replacement_cost = (
+CASE WHEN length BETWEEN 0 AND 100 THEN 10
+			WHEN length > 100 THEN 20
+            END);
