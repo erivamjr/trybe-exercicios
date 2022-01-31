b1c685b6cc81618f4693c628add6e4943638cf4b
José Erivam <erivamdev@gmail.com>
2022-01-31 15:20:57 -0300

    adding exercise of the fixation one 5/5

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-01-21.1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-01-21.1.sql
@@ -13,4 +13,6 @@ SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
 /*3-Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer' .*/
 SELECT CHAR_LENGTH('Uma frase qualquer');
 /*4-Extraia e retorne a palavra "JavaScript" da frase 'A linguagem JavaScript está entre as mais usadas' .*/
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13, 10);
 /*5-Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.*/
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
