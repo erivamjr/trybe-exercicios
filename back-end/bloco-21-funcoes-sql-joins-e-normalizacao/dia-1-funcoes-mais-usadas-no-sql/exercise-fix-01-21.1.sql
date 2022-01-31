c2d7f6ee0ad1960eb28d4abc342e3968c2d08e6b
José Erivam <erivamdev@gmail.com>
2022-01-31 15:12:47 -0300

    adding exercise of the fixation one 2/5

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-01-21.1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-01-21.1.sql
@@ -9,6 +9,7 @@ SELECT SUBSTRING(title, 5) FROM sakila.film WHERE film_id = 1;
 /*1-Faça uma query que exiba a palavra 'trybe' em CAIXA ALTA.*/
 SELECT UCASE('trybe');
 /*2-Faça uma query que transforme a frase 'Você já ouviu falar do DuckDuckGo?' em 'Você já ouviu falar do Google?' .*/
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
 /*3-Utilizando uma query , encontre quantos caracteres temos em 'Uma frase qualquer' .*/
 /*4-Extraia e retorne a palavra "JavaScript" da frase 'A linguagem JavaScript está entre as mais usadas' .*/
 /*5-Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.*/
