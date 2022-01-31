5ba842703802823033449a4eefaff211e779704f
José Erivam <erivamdev@gmail.com>
2022-01-31 18:12:54 -0300

    adding exercise of the fixation two 2/2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-02-21.1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-02-21.1.sql
@@ -10,3 +10,12 @@ PG: "Não recomendado para menores de 10 anos";
 PG-13: "Não recomendado para menores de 13 anos";
 R: "Não recomendado para menores de 17 anos";
 Se não cair em nenhuma das classificações anteriores: "Proibido para menores de idade".*/
+SELECT title, rating,
+CASE 
+WHEN rating LIKE 'G' THEN 'Livre para todos'
+WHEN rating LIKE 'PG' THEN 'Não recomendado para menores de 10 anos'
+WHEN rating LIKE 'PG-13' THEN 'Não recomendado para menores de 13 anos'
+WHEN rating LIKE 'R' THEN 'Não recomendado para menores de 17 anos'
+ELSE 'Proibido para menores de idade'
+END AS 'público-alvo'
+FROM sakila.film;
