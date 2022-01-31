400e1a97191eda36699c575b216ec1dc0bdec9a7
José Erivam <erivamdev@gmail.com>
2022-01-31 18:29:01 -0300

    adding exercise of the fixation three 2/3 about DIV and MOD

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-03-21.1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-03-21.1.sql
@@ -3,4 +3,5 @@ Dica: Números pares são aqueles que podem ser divididos em duas partes iguais.
 1-Monte uma query usando o MOD juntamente com o IF para descobrir se o valor 15 é par ou ímpar. Chame essa coluna de 'Par ou Ímpar' , onde ela pode dizer 'Par' ou 'Ímpar'.*/
 SELECT IF (15 MOD 2 = 0, 'Par','Ímpar') AS 'Par ou Ímpar';
 /*2-Temos uma sala de cinema que comporta 220 pessoas. Quantos grupos completos de 12 pessoas podemos levar ao cinema sem que ninguém fique de fora?*/
+SELECT (220 DIV 12) AS 'Grupo Completos';
 /*3-Utilizando o resultado anterior, responda à seguinte pergunta: temos lugares sobrando? Se sim, quantos?*/
