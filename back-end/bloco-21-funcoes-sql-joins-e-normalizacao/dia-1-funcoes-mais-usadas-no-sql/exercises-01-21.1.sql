2e1d0f857034327dcd6a55e66ea4814806e77990
José Erivam <erivamdev@gmail.com>
2022-02-01 11:17:31 -0300

    adding exercise one 20.1 2/18

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercises-01-21.1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercises-01-21.1.sql
@@ -1,6 +1,7 @@
 -- 1. Escreva uma query que exiba o maior salário da tabela employees.
-SELECT FIRST_NAME, MAX(SALARY) FROM hr.employees;
+SELECT FIRST_NAME, LAST_NAME, MAX(SALARY) FROM hr.employees;
 -- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
+SELECT MAX(SALARY) - MIN(SALARY) FROM hr.employees;
 -- 3. Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em ordem decrescente.
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
