775e7b65d2dbd77cf9e121a04b2f4fb656583e95
José Erivam <erivamdev@gmail.com>
2022-02-01 10:27:47 -0300

    adding exercise of the fixation seven about to pratice GROUP BY 2/4

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-07-21.1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-07-21.1.sql
@@ -3,5 +3,10 @@
 SELECT active, COUNT(*) FROM sakila.customer
 GROUP BY active;
 /*2-Monte uma query para a tabela sakila.customer que exiba a quantidade de clientes ativos e inativos por loja. Os resultados devem conter o ID da loja , o status dos clientes (ativos ou inativos) e a quantidade de clientes por status .*/
+SELECT store_id, active, COUNT(*) FROM sakila.customer
+GROUP BY store_id, active;
 /*3-Monte uma query que exiba a média de duração de locação por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.*/
+SELECT conluna, COUNT(*) FROM sakila.customer
+GROUP BY rating
+ORDER BY rating DESC;
 /*4-Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de endereços registrados nele. Os resultados devem ser ordenados da maior quantidade para a menor.*/
