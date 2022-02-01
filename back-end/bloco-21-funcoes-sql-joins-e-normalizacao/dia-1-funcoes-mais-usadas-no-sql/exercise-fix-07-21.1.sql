95931a23c47ac6a2ea92926375a6aec48bf3d51b
José Erivam <erivamdev@gmail.com>
2022-02-01 10:32:51 -0300

    adding exercise of the fixation seven about to pratice GROUP BY 3/4

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-07-21.1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise-fix-07-21.1.sql
@@ -6,7 +6,7 @@ GROUP BY active;
 SELECT store_id, active, COUNT(*) FROM sakila.customer
 GROUP BY store_id, active;
 /*3-Monte uma query que exiba a média de duração de locação por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor.*/
-SELECT conluna, COUNT(*) FROM sakila.customer
+SELECT rating, AVG(rental_duration) AS media FROM sakila.film
 GROUP BY rating
-ORDER BY rating DESC;
+ORDER BY media DESC;
 /*4-Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de endereços registrados nele. Os resultados devem ser ordenados da maior quantidade para a menor.*/
