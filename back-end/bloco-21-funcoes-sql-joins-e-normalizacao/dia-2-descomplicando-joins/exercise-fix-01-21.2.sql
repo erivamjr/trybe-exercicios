4a72c9cd108f29218021797b2c88ad3b1a2b85f2
José Erivam <erivamdev@gmail.com>
2022-02-02 09:42:59 -0300

    adding exercise of fixation one  about INNER JOIN 4/7

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercise-fix-01-21.2.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercise-fix-01-21.2.sql
@@ -15,13 +15,18 @@ ON staff.address_id = address.address_id;
 -- 3-Exiba o id do cliente , nome e email dos primeiros 100 clientes, ordenados pelo nome em ordem decrescente, juntamente com o id do endereço e o nome da rua onde o cliente mora. Essas informações podem ser encontradas nas tabelas customer e address .
 SELECT c.customer_id, c.first_name, c.last_name, c.email, c.address_id, a.address
 FROM sakila.customer AS c
-INNER JOIN sakila.address as a
+INNER JOIN sakila.address AS a
 ON c.address_id = a.address_id
 ORDER BY c.first_name DESC
 LIMIT 100;
 
--- 4-Exiba o nome , email , id do endereço , endereço e distrito dos clientes que moram no distrito da California e que contêm "rene" em seus nomes. As informações podem ser encontradas nas tabelas address e customer .
-
+-- 4-Exiba o nome , email , id do endereço , endereço e distrito dos clientes que moram no distrito da California e que contêm "rene" em seus nomes. As informações podem ser encontradas nas tabelas address e customer . 
+SELECT first_name, email, c.address_id, address, district
+-- testando com resumo de identificações 
+FROM sakila.customer AS c
+INNER JOIN sakila.address AS a
+ON c.address_id = a.address_id
+WHERE first_name LIKE '%rene%' AND district LIKE 'California';
 -- 5-Exiba o nome e a quantidade de endereços dos clientes cadastrados. Ordene seus resultados por nomes de forma decrescente. Exiba somente os clientes ativos. As informações podem ser encontradas na tabela address e customer .
 -- 6-Monte uma query que exiba o nome , sobrenome e a média de valor ( amount ) paga aos funcionários no ano de 2006. Use as tabelas payment e staff . Os resultados devem estar agrupados pelo nome e sobrenome do funcionário.
 -- 7-Monte uma query que exiba o id do ator , nome , id do filme e título do filme , usando as tabelas actor , film_actor e film . Dica: você precisará fazer mais de um JOIN na mesma query .
