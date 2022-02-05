8a66b1d0320e20132b87dec9f7a749038e16ea58
José Erivam <erivamdev@gmail.com>
2022-02-05 20:15:01 -0300

    adding exercise one

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -0,0 +1,9 @@
+
+/* 
+1- Crie um script para calcular o Índice de Massa Corporal(IMC) de uma pessoa.
+    1-A fórmula para calcular o IMC é peso / altura ^ 2 . Obs: Lembre-se que a altura é em metros, caso deseje usar em centímetros a conversão para metros será necessária.
+    2-Comece criando um novo pacote node com npm init e respondendo às perguntas do npm .
+    3-Por enquanto, não se preocupe em pedir input da pessoa usuária. Utilize valores fixos para peso e altura .
+    4-Armazene o script no arquivo imc.js . */
+    const imcCalculator = (weight,height) => (weight / Math.pow(height, 2)).toFixed(2);
+    
