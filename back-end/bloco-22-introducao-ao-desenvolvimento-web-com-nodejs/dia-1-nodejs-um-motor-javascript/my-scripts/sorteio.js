8bb62710755035a274dd5a4adb388d7dc8101b30
José Erivam <erivamdev@gmail.com>
2022-02-06 17:19:43 -0300

    finished

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/sorteio.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/sorteio.js
@@ -1,23 +1,22 @@
 const readline = require('readline-sync');
 
-/*7-Crie um "jogo de adivinhação" em que a pessoa ganha se acertar qual foi o número aleatório gerado
+/* 7-Crie um "jogo de adivinhação" em que a pessoa ganha se acertar qual foi o número aleatório gerado
         1-O script deve ser executado através do comando npm run sorteio .
         2-Utilize o readline-sync para realizar input de dados.
         3-Armazene o script em sorteio.js .
         4-O número gerado deve ser um inteiro entre 0 e 10.
         5-Caso a pessoa acerte o número, exiba na tela "Parabéns, número correto!".
-        6-Caso a pessoa erre o número, exiba na tela "Opa, não foi dessa vez. O número era [número sorteado]".*/
+        6-Caso a pessoa erre o número, exiba na tela "Opa, não foi dessa vez. O número era [número sorteado]". */
 
 const randomGenerator = () => {
-    let condition = 's'
+    let condition = 's';
     
-    while (condition.toLocaleLowerCase().includes('s')){
-    
+    while (condition.toLocaleLowerCase().includes('s')) {
         const number = readline.questionInt('Insira um numero de 1 a 10? ');
         const randomNumber = Math.floor(Math.random() * 1);
-        if(number == randomNumber){
-            console.log(`Parabéns, número correto!`); 
-        }else{
+        if (number === randomNumber) {
+            console.log('Parabéns, número correto!'); 
+        } else {
             console.log(`Opa, não foi dessa vez. O número era ${randomNumber}`);
         }
         condition = readline.question('Deseja jogar novamente, sim ou não? ');          
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/velocidade.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/velocidade.js
@@ -5,14 +5,14 @@ const readline = require('readline-sync');
         3-Agora, permita que o script seja executado através do comando npm run velocidade . Para isso, crie a chave velocidade dentro do objeto scripts no package.json .
         4-Utilize o readline-sync para solicitar os dados à pessoa.
         5-Considere a distância em metros e o tempo em segundos. Repare que, agora, estamos trabalhando com números inteiros. */
-const speedMediaCalculator = (meters,time) => (meters / time).toFixed(2);
+const speedMediaCalculator = (meters, time) => (meters / time).toFixed(2);
 
 const meters = readline.questionInt('Quantos metros percorridos? ');
 const time = readline.questionInt('Quantos segundos? ');
 
 const printResult = () => {
-        console.log(`Velocidade média de ${speedMediaCalculator(meters,time)} m/s`);
-}
+        console.log(`Velocidade média de ${speedMediaCalculator(meters, time)} m/s`);
+};
 
 printResult();
 module.exports = printResult;
