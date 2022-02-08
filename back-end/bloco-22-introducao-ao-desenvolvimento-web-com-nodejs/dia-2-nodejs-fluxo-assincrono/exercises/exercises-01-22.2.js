b9ca4ec6c266dcca675e2014b598d346890e538b
José Erivam <erivamdev@gmail.com>
2022-02-08 20:35:06 -0300

    adding exercise three

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/exercises-01-22.2.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercises/exercises-01-22.2.js
@@ -19,11 +19,35 @@ const multipliqueNumber = (a, b, c) => {
 
   return promise;
 }
+/*
+2-Escreva um código para consumir a função construída no exercício anterior.
+    1-Gere um número aleatório de 1 a 100 para cada parâmetro que a função recebe. Para gerar um número aleatório, utilize o seguinte trecho de código: Math.floor(Math.random() * 100 + 1) .
+    2-Chame a função do exercício anterior, passando os três números aleatórios como parâmetros. */
 
 const number1 = Number(Math.floor(Math.random() * 100 + 1));
 const number2 = Number(Math.floor(Math.random() * 100 + 1));
 const number3 = Number(Math.floor(Math.random() * 100 + 1));
 console.log(number1, number2, number3);
-multipliqueNumber(number1, number2, number3)
+
+/* 
+3- Utilize then e catch para manipular a Promise retornada pela função:
+    1-Caso a Promise seja rejeitada, escreva na tela o motivo da rejeição.
+    2-Caso a Promise seja resolvida, escreva na tela o resultado do cálculo.
+    3-Reescreva o código do exercício anterior para que utilize async/await .
+Lembre-se: a palavra chave await só pode ser utilizada dentro de funções async . 
+*/
+
+/* multipliqueNumber(number1, number2, number3)
   .then((result) => console.log(`sucesso: ${result}`))
-  .catch((error) => console.log(`erro: ${error}`));
+  .catch((error) => console.log(`erro: ${error}`)); */
+
+const main = async () => {
+  try {
+    const result = await multipliqueNumber(number1, number2, number3)
+    console.log(result);
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  }
+}
+
+main();
