db59047b319bddfa1fa5051097c54ff74142f7a9
José Erivam <erivamdev@gmail.com>
2022-02-13 18:45:58 -0300

    adding exercise of the fixation two

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/exercise-fix-01-22.4.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/exercise-fix-01-22.4.js
@@ -2,18 +2,21 @@ const express = require('express');
 const res = require('express/lib/response');
 
 const app = express();
-
+// 1-Crie uma array drinks com os seguintes objetos dentro dela e uma rota GET /drinks que retorna a lista de bebidas.
 const drinks = [
   { id: 1, name: 'Refrigerante Lata', price: 5.0 },
   { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
   { id: 3, name: 'Suco 300ml', price: 4.0 },
   { id: 4, name: 'Suco 1l', price: 10.0 },
   { id: 5, name: 'Cerveja Lata', price: 4.5 },
-  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+  { id: 6, name: 'Agua Mineral 500 ml', price: 5.0 },
 ];
 
+const sortDrinks = drinks.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0);
+console.log(sortDrinks);
+// 2-Modifique tanto a rota de bebidas como de receitas para retornar a lista ordenada pelo nome em ordem alfabética.
 app.get('/drinks', (req, res) => {
-  return res.json(drinks);
+  return res.json(sortDrinks);
 })
 
 app.listen(3001, (req, res) => {
