504c365ed79f96a1e8a10f72f4abb213f03b9ebc
José Erivam <erivamdev@gmail.com>
2022-02-14 15:02:07 -0300

    adding exercise of fixation two

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/exercise-fix-01-22.4.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/hello-express/exercise-fix-01-22.4.js
@@ -12,14 +12,23 @@ const drinks = [
   { id: 6, name: 'Agua Mineral 500 ml', price: 5.0 },
 ];
 
-const sortDrinks = drinks.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0);
-console.log(sortDrinks);
 // 2-Modifique tanto a rota de bebidas como de receitas para retornar a lista ordenada pelo nome em ordem alfabética.
 app.get('/drinks', (req, res) => {
+  const sortDrinks = drinks.sort((a, b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0);
+  console.log(sortDrinks);
   return res.json(sortDrinks);
 })
 
 app.listen(3001, (req, res) => {
   console.log('Aplicação ouvindo na porta 3001');
+});
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = drinks.find((r) => r.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!' });
+
+  res.status(200).json(recipe);
 });
 
