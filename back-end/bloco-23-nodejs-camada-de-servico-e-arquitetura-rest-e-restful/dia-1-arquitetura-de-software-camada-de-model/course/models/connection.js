eade70bd2d0508da15d560c551d5c843515c7293
José Erivam <erivamdev@gmail.com>
2022-02-18 13:43:10 -0300

    adding course

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/course/models/connection.js
@@ -0,0 +1,10 @@
+// importando o modulo de mysql e como vai trabalhar com assincronicidade colocamos outro modulo /promise
+const mysql = require('mysql2/promise')
+
+// criando um objeto que retorna uma função para trabalhar com multiconexões
+const connection = mysql.createPool({
+  user: 'root',
+  password: 'Trybe123',
+  host: 'localhost',
+  database: 'model_example'
+})
