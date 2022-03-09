0ca6a2be8bf88a545a7fbc006dfa4cbed62bbd79
José Erivam <erivamdev@gmail.com>
2022-03-08 23:37:02 -0300

    adding function get by id

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projectExercise/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projectExercise/index.js
@@ -5,15 +5,29 @@ const app = express();
 
 app.use(express.json());
 console.log(BooksModel);
-app.get('/', (req, res) => {
-  BooksModel.findAll().then(books => {
-    console.log('teste', books);
-    res.status(200).json(books);
-  }).catch(err => {
-    console.log(err.message)
-    res.status(500).json({ message: 'Algo deu errado!' })
-  })
+app.get('/books', async (req, res) => {
+  try {
+    const books = await BooksModel.findAll({ order: [['title', 'ASC'], ['createdAt', 'ASC']] }); // Bônus: Crie uma ordenação no endpoint GET /books para ordenar por ordem alfabética e por data de criação;
 
+    return res.status(200).json(books);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+
+})
+
+app.get('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const books = await BooksModel.findByPk(id);
+
+    res.status(200);
+    res.json(books);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 })
 const PORT = 3000;
 
