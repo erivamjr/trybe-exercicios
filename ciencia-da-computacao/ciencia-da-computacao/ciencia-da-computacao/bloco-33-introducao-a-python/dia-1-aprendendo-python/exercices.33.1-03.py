f522fd4194c2bea461d233c01e1c6f762b26fdc9
José Erivam <erivamdev@gmail.com>
2022-06-30 14:16:31 -0300

    :sparkles: feat:add fuction of asterisks in python

---
+++ ciencia-da-computacao/ciencia-da-computacao/ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercices.33.1-03.py
@@ -0,0 +1,19 @@
+# Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na
+# tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+# n = 5
+# *****
+# *****
+# *****
+# *****
+# *****
+
+def asterisks(n):
+    if n > 1:
+        for i in range(n):
+            print(n)
+            print("*" * n)
+    else:
+        print('Number have to be greater than 1')
+
+
+asterisks(1)
